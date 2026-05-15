c4dda89bdbb98319d7d15c8fb79f1f0e897c662f
EmilySidlow <eas155@duke.edu>
2026-05-15 10:50:43 -0400

    further edited list

--- grocery.R
+++ grocery.R
@@ -1,11 +1,7 @@
 # tiramsu
 # mint chip
-# creme bruele
 # stainless steel ice cream and cookie
 # almond croissant
 # anything chocolate
 # fruit flavored ice cream
-# creme brule
-
-
-# awesome
+# creme bruele
